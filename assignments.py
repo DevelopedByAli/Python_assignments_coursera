132c5e110ec01aa8ade4fc027235e06b5bf078d7
Akbarali <akbarali2191@gmail.com>
2020-09-16 19:57:17 +0200

    Update assignments.py

--- assignments.py
+++ assignments.py
@@ -1,9 +1,11 @@
-# assignment number 1
+# assignment number 1 (2.1)
 name = input("Enter your name")
 print("Hello", name)
 
-# assignment number 2
+# assignment number 2 (2.2)
 hrs = input("Enter Hours:")
 rate = input("Enter Rate:")
 x = float(hrs)*float(rate)
 print("Pay:", x)
+
+# assignment number 3 
