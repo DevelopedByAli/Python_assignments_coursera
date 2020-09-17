3876597ab351b5ef7b713375292403d0680c5244
Akbarali <akbarali2191@gmail.com>
2020-09-17 16:21:56 +0200

    Update assignments.py

--- assignments.py
+++ assignments.py
@@ -16,3 +16,19 @@ r = float(rate)
 if h > 40 : pay = h * r
   else : pay = h * r + 5 * 1.5 * r
     print("Pay:", pay)
+    
+    
+score = input("Enter Score between 0.0 and 1.0: ")
+s = float(score)
+if 0.0 <= s < 0.6:
+    print ("F")
+    elif 0.6 <= s < 0.7:
+        print("D")
+    elif 0.7 <= s < 0.8:
+        print("C")
+    elif 0.8 <= s < 0.9:
+        print("B")
+    elif 0.9 <= s < 1.0:
+        print("A")
+    else print("error")
+            
