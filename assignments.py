ece932cd0c20a65c15e2932031f32ea7cda0daf0
Akbarali <akbarali2191@gmail.com>
2020-09-26 21:02:08 +0200

    Update assignments.py

--- assignments.py
+++ assignments.py
@@ -101,3 +101,9 @@ for line in fh:
 print("Average spam confidence:", sm / count)    
 
 # Assignment number 10 (8.4) (According to a theoty of lists)
+
+# 8.4 Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. 
+# The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. 
+# When the program completes, sort and print the resulting words in alphabetical order.
+
+
