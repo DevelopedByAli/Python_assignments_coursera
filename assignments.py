bd00e3d934160914f3194bb7cf0e2f68c1f82c94
Akbarali <akbarali2191@gmail.com>
2020-09-27 15:05:30 +0200

    Update assignments.py

--- assignments.py
+++ assignments.py
@@ -124,5 +124,5 @@ print(lst)
 # Hint: make sure not to include the lines that start with 'From:'. Also look at the last line of the sample output to see how to print the count.
 fname = input("Enter file name: ")
 if len(fname) < 1 : fname = "mbox-short.txt"
-
+fh = open(fname)
 
