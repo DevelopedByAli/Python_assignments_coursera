7a6e4d58c6bb19965c9f073bb672922731e79e83
Akbarali <akbarali2191@gmail.com>
2020-10-03 14:29:18 +0200

    Update extra.py

--- extra.py
+++ extra.py
@@ -1,4 +1,4 @@
-# Assignment number 14, Python for everybody, Coursera
+# Assignment number 14, Extracting Data With Regular Expressions, Python for everybody, Coursera
 # Using regular expressions (The Regex for the first time)
 # In this assignment you will read through and parse a file with text and numbers. You will extract all the numbers in the file and compute the sum of the numbers.
 
