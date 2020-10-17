b2ca5f6511d8848c02f673c3d499a696755d4eac
Akbarali <akbarali2191@gmail.com>
2020-10-17 14:28:28 +0200

    Update extra.py

--- extra.py
+++ extra.py
@@ -173,5 +173,10 @@ print('summe: ', summe)
 # To complete this assignment, you should use this API endpoint that has a static subset of the Google Data:
 
 http://py4e-data.dr-chuck.net/json?
+  
+This API uses the same parameter (address) as the Google API. This API also has no rate limit so you can test as often as you like. If you visit the URL with no parameters, you get "No address..." response.
+To call the API, you need to include a key= parameter and provide the address that you are requesting as the address= parameter that is properly URL encoded using the urllib.parse.urlencode() function as shown in http://www.py4e.com/code3/geojson.py
+
+Make sure to check that your code is using the API endpoint is as shown above. You will get different results from the geojson and json endpoints so make sure you are using the same end point as this autograder is using.
 
 
