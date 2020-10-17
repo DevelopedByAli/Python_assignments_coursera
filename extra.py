50c22a5134a638e1a153aac97ebb79fb3ca93562
Akbarali <akbarali2191@gmail.com>
2020-10-17 14:26:49 +0200

    Update extra.py

--- extra.py
+++ extra.py
@@ -165,4 +165,11 @@ print('summe: ', summe)
 
 # Assignment number 20  # Using the GeoJSON API
 
+# Calling a JSON API
+
+# In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/geojson.py. The program will prompt for a location, contact a web service and retrieve JSON for the web service and parse that data, and retrieve the first place_id from the JSON. A place ID is a textual identifier that uniquely identifies a place as within Google Maps.
+# API End Points
+
+# To complete this assignment, you should use this API endpoint that has a static subset of the Google Data:
+
 
