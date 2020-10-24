f6ba2496246433827d6d0c95c7cf7e68796075c4
Akbarali <akbarali2191@gmail.com>
2020-10-24 19:25:23 +0200

    Update extra.py

--- extra.py
+++ extra.py
@@ -211,3 +211,8 @@ import sqlite3
 
 conn = sqlite3.connect('emaildb.sqlite')
 cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (email TEXT, count INTEGER)''')
