f9c8d30e7a2874136f6ab6e1c431c13fbe8d6c13
Akbarali <akbarali2191@gmail.com>
2020-10-25 14:35:42 +0100

    Update extra.py

--- extra.py
+++ extra.py
@@ -241,4 +241,30 @@ for row in cur.execute(sqlstr):
 
 cur.close()
 
+# Musical Track Database
+# This application will read an iTunes export file in XML and produce a properly normalized database with this structure:
+'''CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Album (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    artist_id  INTEGER,
+    title   TEXT UNIQUE
+);
+
+CREATE TABLE Track (
+    id  INTEGER NOT NULL PRIMARY KEY 
+        AUTOINCREMENT UNIQUE,
+    title TEXT  UNIQUE,
+    album_id  INTEGER,
+    genre_id  INTEGER,
+    len INTEGER, rating INTEGER, count INTEGER
+);'''
 
