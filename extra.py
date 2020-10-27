a4b20ecc9367310bb5d2bf6cfb52b004a12508c6
Akbarali <akbarali2191@gmail.com>
2020-10-27 16:38:15 +0100

    Update extra.py

--- extra.py
+++ extra.py
@@ -334,3 +334,46 @@ def lookup(d, key):
         if child.tag == 'key' and child.text == key :
             found = True
     return None
+
+stuff = ET.parse(fname)
+all = stuff.findall('dict/dict/dict')
+print('Dict count:', len(all))
+for entry in all:
+    if ( lookup(entry, 'Track ID') is None ) : continue
+
+
+    artist = lookup(entry, 'Artist')
+    genre = lookup(entry, 'Genre')
+    album = lookup(entry, 'Album')
+
+    name = lookup(entry, 'Name')
+    length = lookup(entry, 'Total Time')
+    rating = lookup(entry, 'Rating')
+    count = lookup(entry, 'Play Count')
+
+    if artist is None or genre is None or album is None or name is None :
+        continue
+
+    print(name, artist, album, count, rating, length)
+
+    cur.execute('''INSERT OR IGNORE INTO Artist (name)
+        VALUES ( ? )''', ( artist, ) )
+    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+    artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Genre (name)
+        VALUES ( ? )''', ( genre, ) )
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id)
+        VALUES ( ?, ? )''', ( album, artist_id ) )
+    cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
+    album_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Track
+        (title, album_id, genre_id, len, rating, count)
+        VALUES ( ?, ?, ?, ?, ?, ? )''',
+        ( name, album_id, genre_id, length, rating, count ) )
+
+    conn.commit()
