a0800c2d955dc6cf5367628cef4ff793a0d0003b
Akbarali <akbarali2191@gmail.com>
2020-10-27 16:37:10 +0100

    Update extra.py

--- extra.py
+++ extra.py
@@ -321,3 +321,16 @@ CREATE TABLE Track (
 );
 ''')
 
+fname = input('Enter file name: ')
+if ( len(fname) < 1 ) : fname = 'Library.xml'
+
+# <key>Track ID</key><integer>369</integer>
+# <key>Name</key><string>Another One Bites The Dust</string>
+# <key>Artist</key><string>Queen</string>
+def lookup(d, key):
+    found = False
+    for child in d:
+        if found : return child.text
+        if child.tag == 'key' and child.text == key :
+            found = True
+    return None
